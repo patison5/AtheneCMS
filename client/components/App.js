d649d2f8134a223607e37ef1ac1bc419092ae3a7
patison5 <patison4@yandex.ru>
2019-01-05 18:28:48 +0300

    Add react to project

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment  } from 'react'
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
+
+class App extends React.Component {
+    render () {
+        return (
+            <h1>Hello world!</h1>
+        );
+    }
+
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = (env, argv) => ({
+	entry: `${__dirname}/client/index`,
+	output: {
+		path: `${__dirname}/public/js`,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
+				loader: 'file-loader',
+				options: {
+				  name(file) {
+				    if (argv.mode === 'development') {
+				      return '[path][name].[ext]';
+				    }
+
+				    return '[hash].[ext]';
+				  },
+				},
+			}
+		]
+	},
+	devtool: argv.mode === 'development' ? 'source-map' : false
+})
